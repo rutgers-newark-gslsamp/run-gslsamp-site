8da80690a7cfed24d605873b6abc27da92a452d1
Nicholas Rios <rios.nicholas@protonmail.com>
2023-05-03 23:55:48 -0400

    Blur background when mobile nav is toggled

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
     /* MOBILE HAMBURGER MENU */
     const hamburgerMenu = document.querySelector('.bottom-nav-container-mobile');
     const hamburgerMenuBars = document.querySelector('.hamburger-menu-bars');
-    const pageContent = document.querySelector('body > *:not(nav)'); // all content except nav
+    const pageContent = document.querySelector('body > *:not(nav.mobile-nav)'); // all content except nav
 
-    const ToggleHamburgerMenu = () => {
+    const toggleHamburgerMenu = () => {
         if (hamburgerMenu.style.display === 'none') {
-            // makes the hamburger menu visible (hidden by default)
-            hamburgerMenu.style.display = 'flex'; 
+          // makes the hamburger menu visible (hidden by default)
+            hamburgerMenu.style.display = 'flex';
             pageContent.style.filter = 'blur(2px)';
         } else {
-            hamburgerMenu.style.display = 'none'; 
+            hamburgerMenu.style.display = 'none';
             pageContent.style.filter = 'none';
         }
-    }
+      };
     hamburgerMenuBars.addEventListener('click', ToggleHamburgerMenu); 
 
     // Close the hamburger menu when the user clicks outside of it
