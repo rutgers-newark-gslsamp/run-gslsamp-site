4cf4b1ce5b0408e080ed8180e5b7918d16916666
Nicholas Rios <rios.nicholas@protonmail.com>
2023-05-03 23:01:00 -0400

    Debug mobile hamburger menu toggle - Execute JavaScript code after HTML using 'DOMContentLoaded' event listener - Clicking the hamburger menu bars toggles between display property of 'none' and 'flex'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
-/* MOBILE HAMBURGER MENU */
-const hamburgerMenu = document.querySelector('.bottom-nav-container-mobile');
-const hamburgerMenuBars = document.querySelector('#hamburger-menu-bars');
+// Executes JS code after HTML page
+document.addEventListener("DOMContentLoaded", function() {
+    /* MOBILE HAMBURGER MENU */
+    const hamburgerMenu = document.querySelector('.bottom-nav-container-mobile');
+    const hamburgerMenuBars = document.querySelector('.hamburger-menu-bars');
 
-const toggleHamburgerMenu = () => {
-    if (hamburgerMenu.style.display === 'none') {
-        hamburgerMenu.style.display = 'flex';
-        // makes the hamburger menu visible (hidden by default)
-    } else {
+    const toggleHamburgerMenu = () => {
+        if (hamburgerMenu.style.display === 'none') {
+            hamburgerMenu.style.display = 'flex';
+            // makes the hamburger menu visible (hidden by default)
+        } else {
+            hamburgerMenu.style.display = 'none';
+        }
+    }
+    hamburgerMenuBars.addEventListener('click', toggleHamburgerMenu); 
+
+    // Close the hamburger menu when the user clicks outside of it
+    document.addEventListener('click', (event) => {
+        if (!event.target.closest('.mobile-nav')) {
         hamburgerMenu.style.display = 'none';
-    }
-}
-hamburgerMenuBars.addEventListener('click', toggleHamburgerMenu); 
-
-// Close the hamburger menu when the user clicks outside of it
-document.addEventListener('click', (event) => {
-    if (!event.target.closest('.mobile-nav')) {
-      hamburgerMenu.style.display = 'none';
-    }
-  });
+        }
+    });
+});
