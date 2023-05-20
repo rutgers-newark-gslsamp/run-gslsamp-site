d0e56e45910c326ebea405db874ffacd4b6fa5c7
Nicholas Rios <rios.nicholas@protonmail.com>
2023-05-20 19:27:31 -0400

    Implement mobile nav with media query - Screens less than 768px of width will have the mobile nav activated. - Mobile nav is activated by changing its display attribute to 'flex' and the desktop nav display to 'none'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
 // Executes JS code after HTML page
 document.addEventListener("DOMContentLoaded", function() {
-    // /* MOBILE HAMBURGER MENU */
-    // const hamburgerMenu = document.querySelector('.bottom-nav-container-mobile');
-    // const hamburgerMenuBars = document.querySelector('.hamburger-menu-bars');
-    // const pageContent = document.querySelector('body > *:not(nav.mobile-nav)'); // all content except nav
+    /* MOBILE HAMBURGER MENU */
+    const hamburgerMenu = document.querySelector('.bottom-nav-container-mobile');
+    const hamburgerMenuBars = document.querySelector('.hamburger-menu-bars');
+    const pageContent = document.querySelector('body > *:not(nav.mobile-nav)'); // all content except nav
 
-    // const ToggleHamburgerMenu = () => {
-    //     if (hamburgerMenu.style.display === 'none') {
-    //         // makes the hamburger menu visible (hidden by default)
-    //         hamburgerMenu.style.display = 'flex'; 
-    //         pageContent.style.filter = 'blur(2px)';
-    //     } else {
-    //         hamburgerMenu.style.display = 'none'; 
-    //         pageContent.style.filter = 'none';
-    //     }
-    // }
-    // hamburgerMenuBars.addEventListener('click', ToggleHamburgerMenu); 
+    const ToggleHamburgerMenu = () => {
+        if (hamburgerMenu.style.display === 'none') {
+            // makes the hamburger menu visible (hidden by default)
+            hamburgerMenu.style.display = 'flex'; 
+            pageContent.style.filter = 'blur(2px)';
+        } else {
+            hamburgerMenu.style.display = 'none'; 
+            pageContent.style.filter = 'none';
+        }
+    }
+    hamburgerMenuBars.addEventListener('click', ToggleHamburgerMenu); 
 
-    // // Close the hamburger menu when the user clicks outside of it
-    // document.addEventListener('click', (event) => {
-    //     if (!event.target.closest('.mobile-nav')) {
-    //     hamburgerMenu.style.display = 'none';
-    //     }
-    // });
+    // Close the hamburger menu when the user clicks outside of it
+    document.addEventListener('click', (event) => {
+        if (!event.target.closest('.mobile-nav')) {
+        hamburgerMenu.style.display = 'none';
+        }
+    });
 
     
     /* COPY TO CLIPBOARD */
