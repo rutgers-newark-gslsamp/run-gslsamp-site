4017762accee02583362d9c2c082477aebf20a94
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-09-13 14:38:24 -0400

    implemented router for nav and fixed tailwindcss config

--- run-gslsamp-site/src/main.jsx
+++ run-gslsamp-site/src/main.jsx
@@ -2,9 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import About from './pages/about.jsx'
+import Contact from './pages/contact.jsx'
+import Events from './pages/events.jsx'
+import Faq from './pages/faq.jsx'
+import Membership from './pages/membership.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "about-us",
+    element: <About />,
+  },
+  {
+    path: "membership",
+    element: <Membership />,
+  },
+  {
+    path: "events-and-opportunities",
+    element: <Events />,
+  },
+  {
+    path: "contact-us",
+    element: <Contact />,
+  },
+  {
+    path: "faq",
+    element: <Faq />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
