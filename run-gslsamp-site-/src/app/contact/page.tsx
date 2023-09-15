b46959290d67fd6c72dd15a7d36cedde47d60884
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-09-15 16:51:53 -0400

    contact form with api

--- run-gslsamp-site-/src/app/contact/page.tsx
+++ run-gslsamp-site-/src/app/contact/page.tsx
@@ -1,11 +1,11 @@
+import ContactForm from "./ContactForm";
+
 export default function Contact() {
     return (
       <>
         <h1 className="text-[3.2em] flex justify-center font-bold">Contact Us!</h1>
         
-        <form>
-          
-        </form>
+        <ContactForm />
       </>
     );
   }
