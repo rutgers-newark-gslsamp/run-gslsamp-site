728b5db89615e36548fe12f1c81c279750358b40
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-09-17 08:53:16 -0400

    finished faq, started events page

--- run-gslsamp-site-/src/app/components/nav.js
+++ run-gslsamp-site-/src/app/components/nav.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import logo from "/public/Rutgers_University_Newark_logo.png"
+import seal from "/public/Rutgers_University_seal.png"
 
 {/**tailwind NOT implemented*/}
 export default function Nav() {
@@ -8,8 +10,8 @@ export default function Nav() {
           <div class="top-nav-header">
             <div class="nav-logo-wrapper">
               {/**UPDATE image src */}
-              <img class="nav-logo" src="" alt="Rutgers Univesity–Newark Logo"/>
-              <img class="nav-logo" src="" alt="Rutgers University–Newark Logo"/>
+              <img class="nav-logo" src={seal} alt="Rutgers Univesity–Newark Logo"/>
+              <img class="nav-logo" src={logo} alt="Rutgers University–Newark Logo"/>
             </div>
             <hr class="nav-vertical-line"/>
             <label class="nav-header-title">Louis Stokes Alliance for Minority Participation</label>
--- run-gslsamp-site-/src/app/events/page.tsx
+++ run-gslsamp-site-/src/app/events/page.tsx
@@ -4,6 +4,19 @@ export default function Events() {
     return (
       <>
         <Title title={"Events & Opportunities"} />
+        <h2 className="text-3xl flex justify-center font-semibold text-gray-400">
+          Check out our Fackbook for Updates and News!
+        </h2>
+        <div className='grid grid-cols-2'>
+          <div className=''>
+            Image
+          </div>
+          <div className=''>
+            events section
+          </div>
+
+        </div>
+
       </>
     );
   }
