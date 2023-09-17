da3936c760762d5c65a2c7913db86dbe9a21f9e1
Nicholas Rios <rios.nicholas@protonmail.com>
2023-09-17 19:47:49 -0400

    Create NavBar component

---
+++ run-gslsamp-site/src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const NavBar: React.FC = () => {
+    return (
+        <nav>
+            <div id='top-nav-container'>
+                    <img alt="Rutgers Univesity–Newark Logo" />
+                    <img alt="Rutgers University–Newark Logo" />
+                <hr id="nav-vertical-line" />
+                <label>Louis Stokes Alliance for Minority Participation</label>  
+            </div>
+            <div id='bottom-nav-container'>
+                <ul id="bottom-nav-page-list">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About Us</a></li>
+                    <li><a href="/membership">Membership</a></li>
+                    <li><a href="/events">Events & Opportunities</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
