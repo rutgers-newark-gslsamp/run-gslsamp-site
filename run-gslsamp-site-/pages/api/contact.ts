e8db72d7831de306439b11c529ae9c1adfa5b3f7
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-09-19 16:55:49 -0400

    comments

--- run-gslsamp-site-/pages/api/contact.ts
+++ run-gslsamp-site-/pages/api/contact.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json(req.body)    
