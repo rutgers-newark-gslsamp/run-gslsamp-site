eaa1a0a457480f819a9b7703eea14eac2b5fd6bc
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-09-29 15:44:26 -0400

    rearranged files

--- run-gslsamp-site-/app/components/title.js
+++
@@ -1,8 +0,0 @@
-export default function Title({title}) {
-    return(
-        <>
-            <h1 className="text-[3.2em] m-[3rem] flex font-bold my-7 drop-shadow-xl">{title}</h1>
-            
-        </>
-    );
-}
--- run-gslsamp-site-/app/membership/page.tsx
+++
@@ -1,9 +0,0 @@
-import Title from "../components/title";
-
-export default function Membership() {
-    return (
-      <>
-        <Title title={"Membership"}/>
-      </>
-    );
-  }
