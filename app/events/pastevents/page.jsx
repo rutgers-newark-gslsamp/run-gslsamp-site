ad881c675d26e7eca1bcb2cdfee0a471a30c2dca
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-10-01 16:40:35 -0400

    missing folder

---
+++ app/events/pastevents/page.jsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
+
+export default function PastEvents(){
+    const [ pastEvent, setPastEvent ] = useState([])
+
+
+    /**QUERY EVENTS */
+    async function getEvents(){
+      const response = await fetch('/api/events', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+
+      //get response json
+      const resp = await response.json()
+
+      //response handling 
+      if(!response.ok){
+        //MISSIING set description to: No new events coming up. Be on the lookout!
+        console.log(resp.msg)
+
+      } else {
+        console.log(resp.responsePast)
+
+        //set event(s)
+        setPastEvent(resp.responsePast)
+      }
+    }
+
+    //run once 
+    useEffect(() => {
+      getEvents()
+    }, [])
+
+
+    return (
+      <>
+        <h1 className="text-[3.2em] m-[3rem] flex font-bold my-7 drop-shadow-xl">PAST EVENTS</h1>
+
+              {
+                pastEvent.map((e) => (
+                <>
+                  <h1 className='underline text-xl p-7 text-[1.7rem]'>{e.title}</h1>
+                  <p className='font-bold px-9 text-[1.15rem]'>{e.location}</p>
+                  <p className='font-bold px-9 text-[1.15rem]'>{e.date}</p>
+                  <p className='px-9 text-[1.30rem]'>
+                    {e.description}
+                  </p>
+                </>
+                ))
+              }   
+    </>
+  )
+}
