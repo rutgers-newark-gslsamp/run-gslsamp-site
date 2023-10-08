fa853cfdfcc806775d0c7aca9229e8038db816ca
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-10-07 23:32:03 -0400

    file

---
+++ app/components/Title.js
@@ -0,0 +1,7 @@
+export default function Title({name}) {
+    return (
+        <>
+            <h1 className="text-[3.2em] m-[3rem] flex font-bold my-7 drop-shadow-xl">{name}</h1>
+        </>
+    )
+}
