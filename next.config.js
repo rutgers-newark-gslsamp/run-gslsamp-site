3800388c5af40554ac5bee41fa267700a4855e45
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-20 20:51:08 -0400

    Resolve module conflicts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+// module.exports = {
+//     nextConfig,
+//     reactDevOverlay: false,
+// }
 
-module.exports = {
-    nextConfig,
-    reactDevOverlay: false,
-}
+module.exports = nextConfig
