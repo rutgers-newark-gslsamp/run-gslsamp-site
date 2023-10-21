b80af35c8311db65f37ad32eb341f6c815095b21
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-20 23:05:42 -0400

    Refactor legacy CSS to Tailwind CSS
    - FIXME: Global CSS that selects tags in the nav bar is not working

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,55 @@
 import React from 'react';
-import RutgersLogo from '../../public/images/rutgers-logos/Rutgers_University_Newark_logo.png';
-import NSFLogo from '../../public/images/lsamp-nsf-logos/NSF-clear.png';
+import Link from "next/link";
+import Image from "next/image";
+
+// IMAGES
+import nsfLogo from '../../public/lsamp-nsf-logos/NSF-clear.png';
+import rutgersLogo from "../../public/Rutgers_University_Newark_logo.png"
+import seal from "../../public/Rutgers_University_seal.png"
 
 const NavBar: React.FC = () => {
     return (
-        <nav>
-            <div id='top-nav-container'>
-                    <img src={RutgersLogo} alt="Rutgers Univesity–Newark Logo" />
-                    <img src={NSFLogo} alt="Rutgers University–Newark Logo" />
-                <hr id="nav-vertical-line" />
-                <label>Louis Stokes Alliance for Minority Participation</label>  
+        <nav className='flex flex-col w-full bg-white sticky z-30 top-0'>
+            <div className='flex flex-wrap items-center justify-between' id='top-nav-container'>
+                <header className='flex items-center'>
+                    <div className='flex mt-1 mb-4' id='nav-logo-wrapper'>
+                        <Image 
+                            className='flex h-14 w-auto py-3 px-2 drop-shadow'
+                            src={rutgersLogo}
+                            alt="Rutgers Univesity–Newark Logo" 
+                            width={500}
+                            height={500}
+                            />
+                        <Image
+                            className='flex h-14 w-auto py-3 px-2 drop-shadow'
+                            src={nsfLogo}
+                            alt="NSF Logo"
+                            width={500}
+                            height={500}
+                            />
+                    </div>
+                    <hr className="text-gray-200 h-10 border-l-2 border-solid border-gray-200" />
+                    <label className='flex ml-4 text-lg font-normal'>Louis Stokes Alliance for Minority Participation</label>  
+                </header>
             </div>
-            <div id='bottom-nav-container'>
-                <ul id="bottom-nav-page-list">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About Us</a></li>
-                    <li><a href="/membership">Membership</a></li>
-                    <li><a href="/events">Events & Opportunities</a></li>
-                    <li><a href="/contact">Contact Us</a></li>
+            <div className='flex justify-center items-center text-center z-30 shadow-md bg-gradient-to-t from-red-900 to-red-700' id='bottom-nav-container'>
+                <ul className='flex text-base justify-center place-items-center flex-wrap text-white font-medium my-1 mx-5 list-none no-underline drop-shadow-md' id="bottom-nav-page-list">
+                    {/* FIXME: GLOBAL CSS FOR HOVER EFFECT IS NOT WORKING */}
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About Us</Link>
+                    </li>
+                    <li>
+                        <Link href="/membership">Membership</Link>
+                    </li>
+                    <li>
+                        <Link href="/events">Events & Opportunities</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">Contact Us</Link>
+                    </li>
                 </ul>
             </div>
         </nav>
