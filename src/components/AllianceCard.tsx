a1d20b82789b00df16ee1351ea49642f8c3a2b60
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-20 22:05:54 -0400

    Translate AllianceCard into TSX functional component
    - Refactored legacy CSS into Tailwind
    - Utilized Next.js Image component

---
+++ src/components/AllianceCard.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+const AllianceCard: React.FC = ({ logo, universityName, contactName, contactEmail, contactPhone }) => {
+    return (
+        <div className="bg-white flex flex-col items-center justify-between align-middle p-1 m-1 max-h-56 max-w-56 shadow rounded-lg hover:bg-gray-300 hover:cursor-copy hover:transition duration-500">
+            <Image
+                className="flex justify-center align-middle object-contain p-1 rounded-lg"
+                src={logo}
+                alt={`${universityName} Logo`}
+                width={100}
+                layout="fixed"
+                height={100}
+            />
+            <div className="h-1/2 text-sm text-center m-0 p-1 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                <h3>{universityName}</h3>
+                <div>
+                    <p>
+                        {contactName}
+                        <br/>
+                        <span>{contactEmail}</span>
+                        <br/>
+                        {contactPhone}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AllianceCard;
