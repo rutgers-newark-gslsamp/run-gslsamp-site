1432a7046bdb8bafcf1e564cbc7a516982b2303e
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-20 23:05:25 -0400

    Update Tailwind CSS

--- src/components/AllianceGrid.tsx
+++ src/components/AllianceGrid.tsx
@@ -15,79 +15,73 @@ import AllianceCard from "./AllianceCard";
 
 const AllianceGrid: React.FC = () => {
     return (
-        <section id="alliance-section">
-            {/* <div className="section-header">
-                <h2 className="lsamp-alliance-header">GS-LSAMP Alliance</h2>
-                <hr className="section-horizontal-line"/>
-            </div> */}
-            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 p-2 m-12 gap-4">
-                <AllianceCard
-                    logo={runLogo}
-                    universityName='Rutgers University-Newark' 
-                    contactName='Mr. Oren Rabinovich' 
-                    contactEmail='oren.rabinovich@rutgers.edu' 
-                    contactPhone='(973) 353-3562'
-                    id="alliance-rutgers-newark"
-                />
-                <AllianceCard
-                    logo={fduLogo}
-                    universityName='Fairleigh Dickinson University' 
-                    contactName='Dr. Marion McClary' 
-                    contactEmail='mcclary@fdu.edu' 
-                    contactPhone='(201) 692-2606 '
-                    id="alliance-fairleigh-dickinson"
-                />
-                <AllianceCard
-                    logo={montclairLogo}
-                    universityName='Montclair State University' 
-                    contactName='Dr. Yvonne Gindt' 
-                    contactEmail='gindty@montclair.edu' 
-                    contactPhone='(973) 655-3469'
-                    id="alliance-montclair"
-                />
-                <AllianceCard
-                    logo={williamPatLogo}
-                    universityName='William Paterson University' 
-                    contactName='Dr. Eliana Antoniou' 
-                    contactEmail='antonioue@wpunj.edu' 
-                    contactPhone='(973) 720-3715'
-                    id='alliance-william-paterson'
-                />
-                <AllianceCard
-                    logo={keanLogo}
-                    universityName='Kean University'
-                    contactName='Dr. Juyoung Ha' 
-                    contactEmail='haj@kean.edu' 
-                    contactPhone='(908) 737-3739'
-                    id='alliance-kean'
-                />
-                <AllianceCard
-                    logo={essexLogo}
-                    universityName='Essex County College' 
-                    contactName='' 
-                    contactEmail='gslsamp@essex.edu' 
-                    contactPhone=''
-                    id="alliance-essex"
-                />
-                <AllianceCard
-                    logo={njitLogo}
-                    universityName='New Jersey Institute of Technology' 
-                    contactName='Dr. Crystal Smith' 
-                    contactEmail='smithcs@njit.edu' 
-                    contactPhone='(973) 596-3690'
-                    id='alliance-njit'
-                />
-                <AllianceCard
-                    logo={runbLogo}
-                    universityName='Rutgers University-New Brunswick' 
-                    contactName='Dr. Beverely Gaines' 
-                    contactEmail='bag104@echo.rutgers.edu' 
-                    contactPhone='(848) 932-2559'
-                    id=''
-                />
-          </div>
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 p-2 mx-6 mb-6 lg:mx-12 gap-4">
+            <AllianceCard
+                logo={runLogo}
+                universityName='Rutgers University-Newark' 
+                contactName='Mr. Oren Rabinovich' 
+                contactEmail='oren.rabinovich@rutgers.edu' 
+                contactPhone='(973) 353-3562'
+                id="alliance-rutgers-newark"
+            />
+            <AllianceCard
+                logo={fduLogo}
+                universityName='Fairleigh Dickinson University' 
+                contactName='Dr. Marion McClary' 
+                contactEmail='mcclary@fdu.edu' 
+                contactPhone='(201) 692-2606 '
+                id="alliance-fairleigh-dickinson"
+            />
+            <AllianceCard
+                logo={montclairLogo}
+                universityName='Montclair State University' 
+                contactName='Dr. Yvonne Gindt' 
+                contactEmail='gindty@montclair.edu' 
+                contactPhone='(973) 655-3469'
+                id="alliance-montclair"
+            />
+            <AllianceCard
+                logo={williamPatLogo}
+                universityName='William Paterson University' 
+                contactName='Dr. Eliana Antoniou' 
+                contactEmail='antonioue@wpunj.edu' 
+                contactPhone='(973) 720-3715'
+                id='alliance-william-paterson'
+            />
+            <AllianceCard
+                logo={keanLogo}
+                universityName='Kean University'
+                contactName='Dr. Juyoung Ha' 
+                contactEmail='haj@kean.edu' 
+                contactPhone='(908) 737-3739'
+                id='alliance-kean'
+            />
+            <AllianceCard
+                logo={essexLogo}
+                universityName='Essex County College' 
+                contactName='' 
+                contactEmail='gslsamp@essex.edu' 
+                contactPhone=''
+                id="alliance-essex"
+            />
+            <AllianceCard
+                logo={njitLogo}
+                universityName='New Jersey Institute of Technology' 
+                contactName='Dr. Crystal Smith' 
+                contactEmail='smithcs@njit.edu' 
+                contactPhone='(973) 596-3690'
+                id='alliance-njit'
+            />
+            <AllianceCard
+                logo={runbLogo}
+                universityName='Rutgers University-New Brunswick' 
+                contactName='Dr. Beverely Gaines' 
+                contactEmail='bag104@echo.rutgers.edu' 
+                contactPhone='(848) 932-2559'
+                id=''
+            />
+        </div>
           
-      </section>
     );
 };
 
