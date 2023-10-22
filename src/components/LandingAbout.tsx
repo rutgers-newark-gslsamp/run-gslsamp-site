b6b4506bf5f50d7d3cdecd62865e1a8d794f9d24
Nicholas Rios <rios.nicholas@protonmail.com>
2023-10-22 14:45:16 -0400

    Refactor legacy CSS to implement TailwindCSS

--- src/components/LandingAbout.tsx
+++ src/components/LandingAbout.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import runLogo from "../../public/rutgers-logos/ru-n_logo.png"
+import Link from "next/link";
 
+
+// TODO: Replace anchor tag with Link component that implements the router  
 const LandingAbout: React.FC = () => {
     return (
         <section className="flex flex-wrap justify-center items-center my-12">
@@ -11,24 +14,27 @@ const LandingAbout: React.FC = () => {
                 alt="Rutgers–Newark GS-LSAMP Logo"
             />  
             
-            <div className="landing-about-card flex flex-col items-center py-3 px-4 ml-10 rounded-3xl shadow-lg max-w-lg shadow-black/20">
-                <div className="landing-about-content flex flex-col items-start text-base">
-                    <h2 className="uppercase text-3xl font-bold my-1 mx-0 pt-1 pb-0 px-1">What is GS-LSAMP?</h2>
-                    <p className="landing-about-body">
+            <div className="flex flex-col items-center py-3 px-4 ml-10 rounded-3xl shadow-lg max-w-lg shadow-black/20">
+                <div className="flex flex-col items-start text-base">
+                    <h2 className="uppercase text-4xl font-bold my-1 px-1">What is GS-LSAMP?</h2>
+                    <p className="p-2 text-base">
                         The Louis Stokes Alliance for Minority Participation (LSAMP) is an 
-                        <a className="landing-about-anchor-body m-1" href="https://www.nsf.gov/" target="blank">
+                        <a className="text-black shadow-none m-1 hover:bg-neutral-300 rounded transition duration-200 font-medium" href="https://www.nsf.gov/" target="blank">
                             <i>NSF-funded</i>
                         </a> 
                         program that provides academic support and specialized advising for students from historically underrepresented groups who wish to enter non-medical STEM professions. For students, adjusting to an unfamiliar environment can prove to be a difficult task. However, LSAMP aims to aid students in navigating critical transitions, including those from high school to college, college to graduate school, and eventually towards fulfilling careers.
                     </p>
                 </div>    
-                <div className="landing-about-button-container">
-                    <button className="landing-about-button" id="landing-about-button-pending">
+                <div className="flex justify-evenly items-center">
+                    <button className="bg-gray-300 border-4 border-gray-300 rounded text-xs max-w-sm max-h-12 mx-1 shadow-sm shadow-black/40">
                         {/*How faculty can help LSAMP*/}
-                        <a className="landing-about-anchor-button"  /*alt="Learn more about GS-LSAMP in our About Us Page" onclick="alert('Coming Soon!')"*/>For Faculty</a>
+                        <a className="text-base text-black font-semibold hover:underline px-2 py-1" href="about.html"  /*alt="Learn more about GS-LSAMP in our About Us Page" onclick="alert('Coming Soon!')"*/>For Faculty</a>
                     </button>
-                    <button className="landing-about-button">
-                        <a className="landing-about-anchor-button" href="about.html" /*alt="Learn more about GS-LSAMP in our About Us Page"*/>Learn More</a>
+                    <button className="bg-gray-50 border-4 border-gray-300 rounded text-xs max-w-sm max-h-12 mx-1 shadow-sm shadow-black/40">
+                        <a className="text-base text-black font-semibold hover:underline px-2 py-1" href="about.html" /*alt="Learn more about GS-LSAMP in our About Us Page"*/>Learn More</a>
+                        {/* <Link>
+                            
+                        </Link> */}
                     </button>
                 </div>
             </div>
