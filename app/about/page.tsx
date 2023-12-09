f00e7abe255469cdb3ac2c7e5fe19675aaa37b11
Nicholas Rios <rios.nicholas@protonmail.com>
2023-12-08 20:54:02 -0500

    Refactoring

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,137 +3,98 @@ import rabinovich from "../../public/staff-pictures/rabinovich.jpeg"
 import miguel from "/public/staff-pictures/miguel.png"
 import gates from "/public/staff-pictures/gates.png"
 import StaffCard from "../../src/components/StaffCard";
+import EventInfoCard from "../../src/components/EventInfoCard";
 
 
 export default function About() {
+  return (
+    <>
+      {/*Title component*/}
+      {/**<Title title={"ABOUT US"} />*/}
+      <main className="about-description-container">
+        <div className="section-header">
+          <h1 className="text-4xl uppercase font-extrabold">About Us</h1>
+          <hr/>
+        </div>
+        <p id="about-paragraph" className="subsection-paragraph">
+          The Louis Stokes Alliance for Minority Participation (LSAMP) program at Rutgers University–Newark is a non-medical science program sponsored by the National Science Foundation. The program is designed to increase the interest, retention, graduation, and success of students from racial and ethnic groups that are historically underrepresented in non-medical (STEM) fields (i.e. Black/African American, Hispanic/Latinx, Native American/Alaskan, Pacific Islander). Now in phase III of funding, the primary focus is to promote undergraduate research and post-baccalaureate studies in STEM fields.  The RUN - GSLSAMP program is one of the nine members of the <a href="" className="inline-link">Garden State Louis Strokes Alliance for Minority Participation (GS-LSAMP)</a>
+        </p>
+      </main>
 
-    return (
-      <>
-        {/*Title component*/}
-        {/**<Title title={"ABOUT US"} />*/}
+      <section className="staff-section-container">
+        <div className="section-subheader">
+          <h2 className="text-3xl font-bold text-neutral-700">Staff</h2>
+          <hr/>
+        </div>
+        <div id="staff-card-container" className="grid md:grid-cols-3 mx-4 md:mx-16">
+          <StaffCard 
+            picture={gates}
+            name="Alexander Gates, PhD"
+            role="Executive Director of GS-LSAMP"
+            university="Rutgers University - Newark"
+            email="agates@newark.rutgers.edu"
+            phone="(973) 353-5034"
+          />
+          <StaffCard 
+            picture={miguel}
+            name="Cristiane San Miguel, Ph.D"
+            role="Assistant Director of GS-LSAMP"
+            university="Rutgers University - Newark"
+            email="csanmig@newark.rutgers.edu"
+            phone="(973) 353-5846"
+          />
+          <StaffCard 
+            picture={rabinovich}
+            name="Oren Rabinovich"
+            role="Director of GS-LSAMP"
+            university="Rutgers University - Newark"
+            email="oren.rabinovich@rutgers.edu"
+            phone="(973) 353-3562"
+          />
+        </div>
+      </section>
 
-        <main className="about-description-container">
-          <div className="section-header">
-              <h1 className="about-us-page-title">About Us</h1>
-              <hr/>
-          </div>
-          <p id="about-paragraph" className="subsection-paragraph">
-            The Louis Stokes Alliance for Minority Participation (LSAMP) program
-            at Rutgers University–Newark is a non-medical science program sponsored by the National Science Foundation. The program is designed to increase the interest, retention, graduation, and 
-            success of students from racial and ethnic groups that are 
-            historically underrepresented in non-medical (STEM) fields 
-            (i.e. Black/African American, Hispanic/Latinx, Native American/Alaskan, Pacific Islander). Now in phase III of funding, the primary focus is to promote undergraduate research and post-baccalaureate studies in STEM fields. 
-            The RUN - GSLSAMP program is one of the nine members of the <a href="about html" className="inline-link">Garden State Louis Strokes Alliance for Minority Participation (GS-LSAMP)</a>
+      <section className="club-section-container">
+        <div className="section-subheader">
+          <h2 className="text-3xl font-bold text-neutral-700">Club Info</h2>
+          <hr/>
+        </div>
+        <div>
+          <p id="club-paragraph" className="subsection-paragraph">
+            The GS-LSAMP Club at Rutgers Newark is a student organization affiliated with GS-LSAMP. Its roots lie in the pursuit of academic excellence in science, technology, engineering, and mathematics (STEM) fields.
           </p>
-        </main>
-
-
-        <div className="staff-section-container">
-          <div className="section-subheader">
-            <h2 className="about-us-page-subtitle">Staff</h2>
-            <hr/>
-          </div>
-          <div className="grid md:grid-cols-3">
-            <StaffCard 
-              picture={gates}
-              name="Alexander Gates, PhD"
-              role="Executive Director of GS-LSAMP"
-              university="Rutgers University - Newark"
-              email="agates@newark.rutgers.edu"
-              phone="(973) 353-5034"
-            />
-            <StaffCard 
-              picture={miguel}
-              name="Cristiane San Miguel, Ph.D"
-              role="Assistant Director of GS-LSAMP"
-              university="Rutgers University - Newark"
-              email="csanmig@newark.rutgers.edu"
-              phone="(973) 353-5846"
-            />
-            <StaffCard 
-              picture={rabinovich}
-              name="Oren Rabinovich"
-              role="Director of GS-LSAMP"
-              university="Rutgers University - Newark"
-              email="oren.rabinovich@rutgers.edu"
-              phone="(973) 353-3562"
-            />
-          </div>
         </div>
-
-        <div className="club-section-container">
-          <div className="section-subheader">
-            <h2 className="about-us-page-subtitle">Club Info</h2>
-            <hr/>
-          </div>
-          <div>
-            <p id="club-paragraph" className="subsection-paragraph">
-              The GS-LSAMP Club at Rutgers Newark is a student organization affiliated with GS-LSAMP. Its roots lie in the pursuit of academic excellence in science, technology, engineering, and mathematics (STEM) fields.
-            </p>
-          </div>
+      </section>
+      <section className="about-events-container">
+        <div className="section-subheader">
+          <h2 className="text-3xl font-bold text-neutral-700">Events</h2>
+          <hr/>
         </div>
-        <div className="about-events-container">
-          <div className="section-subheader">
-              <h2 className="about-us-page-subtitle">Events</h2>
-              <hr/>
-          </div>
-          <div className="row">
-              <div className="col-md-4 mb-5">
-                <div className="card h-100 border-0 shadow zoom">
-                  {/**<img class="card-img-top" src="media/photos/research.jpg" width="300px" height="200px" alt="Research Conference Photo">-*/}
-                  <div className="card-body">
-                    <h3 className="card-title">2nd LSAMP Meeting</h3>
-                    <p className="card-text">
-                      11:30AM - 12:50PM | May 26, 2020 <br/> 
-                      Hill Hall, Room 210 <br/>
-                      Rutgers Newark 
-                    </p>
-                  </div>
-                  {/*<div className="card-footer">
-                        <a href="https://docs.google.com/forms/d/e/1FAIpQLScCKgqYLB5U3uixbzyC1ljbrw7XIyHIrZV8szsDfQTqA6Nbpg/viewform" class="btn btn-primary">RSVP!</a>
-                    </div>*/}
-                </div>
-              </div>
-              <div className="col-md-4 mb-5">
-                <div className="card h-100 border-0 shadow zoom">
-                  {/*<img className="card-img-top" src="media/photos/induction.jpg" width="300px" height="200px" alt="Induction Ceremony Photo">*/}
-                  <div className="card-body">
-                    <h3 className="card-title">3rd LSAMP Meeting</h3>
-                    <p className="card-text">
-                      2:30PM - 3:50PM | June 17, 2020 <br/>
-                      Hill Hall, Room 101 <br/>
-                      Rutgers Newark
-                    </p>
-                  </div>
-                  {/*<div className="card-footer">
-                    <a href="#contact" className="btn btn-primary">Find Out More!</a>
-                  </div>*/}
-                </div>
-              </div>
-              <div className="col-md-4 mb-5">
-                  <div className="card h-100 border-0 shadow zoom">
-                    {/*<img className="card-img-top" src="media/photos/induction.jpg" width="300px" height="200px" alt="Induction Ceremony Photo">*/}
-                    <div className="card-body">
-                      <h3 className="card-title">3rd LSAMP Meeting</h3>
-                      <p className="card-text">
-                        2:30PM - 3:50PM | June 17, 2020 <br/>
-                        Hill Hall, Room 101 <br/>
-                        Rutgers Newark
-                      </p>
-                    </div>
-                  </div>
-        
-    </div>
-    </div>
-    </div>
-
-
-      </>
-
-
-        
-
-        
-    );
+        <div id="event-container" className="grid md:grid-cols-3 mx-4 md:mx-16">
+          <EventInfoCard 
+            title="2nd LSAMP Meeting"
+            time="11:30AM - 12:50PM"
+            date="May 26, 2020"
+            room="Hill Hall, Room 210"
+            location="Rutgers Newark"
+          />
+          <EventInfoCard 
+            title="2nd LSAMP Meeting"
+            time="11:30AM - 12:50PM"
+            date="May 26, 2020"
+            room="Hill Hall, Room 210"
+            location="Rutgers Newark"
+          />
+          <EventInfoCard 
+            title="2nd LSAMP Meeting"
+            time="11:30AM - 12:50PM"
+            date="May 26, 2020"
+            room="Hill Hall, Room 210"
+            location="Rutgers Newark"
+          />
+        </div> 
+      </section>
+    </>
+  );
 }
   
