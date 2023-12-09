757058ff1c599c3ea2859f5a297f1fe24eff0d1d
Nicholas Rios <rios.nicholas@protonmail.com>
2023-12-08 20:48:06 -0500

    Refactor card styling

--- src/components/EventInfoCard.tsx
+++ src/components/EventInfoCard.tsx
@@ -8,22 +8,20 @@ interface EventInfoProps {
 
 const EventInfoCard: React.FC<EventInfoProps> = ({ title, time, date, room, location }) => {
     return (
-        <div className="col-md-4 mb-5">
-            <div className="card h-100 border-0 shadow zoom">
-                {/**<img class="card-img-top" src="media/photos/research.jpg" width="300px" height="200px" alt="Research Conference Photo">-*/}
-                <div className="card-body">
-                    <h3 className="card-title">{title}</h3>
-                    <p className="card-text">
-                        <span>{time} | {date}</span><br/> 
-                        {room}<br/>
-                        {location}
-                    </p>
-                </div>
-                {/*<div className="card-footer">
-                        <a href="https://docs.google.com/forms/d/e/1FAIpQLScCKgqYLB5U3uixbzyC1ljbrw7XIyHIrZV8szsDfQTqA6Nbpg/viewform" class="btn btn-primary">RSVP!</a>
-                    </div>*/}
+        <div className="flex items-center m-2 md:m-4 p-2 bg-neutral-300 h-100 border-0 shadow rounded-lg">
+            {/**<img class="card-img-top" src="media/photos/research.jpg" width="300px" height="200px" alt="Research Conference Photo">-*/}
+            <div>
+                <h3 className="text-lg font-bold">{title}</h3>
+                <p>
+                    <span className="font-medium">{time} | {date}</span><br/> 
+                    {room}<br/>
+                    {location}
+                </p>
             </div>
-        </div>      
+            {/*<div className="card-footer">
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLScCKgqYLB5U3uixbzyC1ljbrw7XIyHIrZV8szsDfQTqA6Nbpg/viewform" class="btn btn-primary">RSVP!</a>
+                </div>*/}
+        </div>
     )
 }
 
