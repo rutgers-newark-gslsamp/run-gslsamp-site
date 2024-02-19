4e1e296622c86aa7e06385c9b364872c3de4529a
Nicholas Rios <rios.nicholas@protonmail.com>
2024-02-18 23:12:05 -0500

    Update padding and text sizing

--- src/components/LandingAbout.tsx
+++ src/components/LandingAbout.tsx
@@ -18,8 +18,8 @@ const LandingAbout: React.FC = () => {
             
             <div className="flex flex-col items-center py-3 px-4 ml-10 rounded-3xl shadow-lg max-w-lg shadow-black/20">
                 <div className="flex flex-col items-start text-base">
-                    <h2 className="uppercase text-2xl sm:text-3xl md:text-4xl font-extrabold my-1 px-1">What is GS-LSAMP?</h2>
-                    <p className="p-2 text-sm md:text-base">
+                    <h2 className="uppercase text-2xl md:text-3xl font-extrabold my-1 px-1">What is GS-LSAMP?</h2>
+                    <p className="p-1 md:p-2 text-sm md:text-base">
                         The Louis Stokes Alliance for Minority Participation (LSAMP) is an 
                         <a className="text-black shadow-none m-1 hover:bg-neutral-300 rounded transition duration-200 font-medium" href="https://www.nsf.gov/" target="blank">
                             <i>NSF-funded</i>
@@ -27,7 +27,7 @@ const LandingAbout: React.FC = () => {
                         program that provides academic support and specialized advising for students from historically underrepresented groups who wish to enter non-medical STEM professions. For students, adjusting to an unfamiliar environment can prove to be a difficult task. However, LSAMP aims to aid students in navigating critical transitions, including those from high school to college, college to graduate school, and eventually towards fulfilling careers.
                     </p>
                 </div>    
-                <div className="flex justify-evenly items-center">
+                <div className="flex justify-evenly items-center p-1">
                     <button className="bg-gray-300 border-4 border-gray-300 rounded text-xs max-w-sm max-h-12 mx-1 shadow-sm shadow-black/40">
                         {/*How faculty can help LSAMP*/}
                         <a className="text-base text-black font-semibold hover:underline px-2 py-1" href="/faculty"  /*alt="Learn more about GS-LSAMP in our About Us Page" onclick="alert('Coming Soon!')"*/>For Faculty</a>
