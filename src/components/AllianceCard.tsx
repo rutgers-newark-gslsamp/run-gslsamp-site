4fff1aa13f0ee8f9c52c05608dd657cf3f53463f
Nicholas Rios <rios.nicholas@protonmail.com>
2024-02-23 14:38:37 -0500

    Create conditional rendering for URLs in

--- src/components/AllianceCard.tsx
+++ src/components/AllianceCard.tsx
@@ -4,14 +4,15 @@ import { StaticImageData } from "next/legacy/image";
 interface AllianceCardProps {
     logo: StaticImageData;
     universityName: string;
+    universityURL?: string;
     contactName: string;
     contactEmail: string;
     contactPhone: string;
 }
 
-const AllianceCard: React.FC<AllianceCardProps> = ({ logo, universityName, contactName, contactEmail, contactPhone }) => {
+const AllianceCard: React.FC<AllianceCardProps> = ({ logo, universityName, universityURL, contactName, contactEmail, contactPhone }) => {
     return (
-        <div className="bg-white flex flex-col items-center justify-between align-middle p-1 m-1 max-h-56 max-w-56 shadow rounded-lg hover:bg-gray-300 hover:cursor-copy hover:transition duration-500">
+        <div className="bg-white flex flex-col items-center justify-between align-middle p-1 m-1 max-h-56 max-w-56 shadow rounded-lg hover:bg-gray-300 hover:transition duration-500">
             <Image
                 className="flex justify-center align-middle object-contain p-1 rounded-lg"
                 src={logo}
@@ -21,7 +22,16 @@ const AllianceCard: React.FC<AllianceCardProps> = ({ logo, universityName, conta
                 height={100}
             />
             <div className="h-1/2 text-sm text-center m-0 p-1 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                <h3>{universityName}</h3>
+                {/* if url is null, no cursor click on hover */}
+                {universityURL ? 
+                    <h3>
+                        <a href={universityURL} target="_blank" className="hover:cursor-pointer hover:underline">
+                            {universityName}
+                        </a>
+                    </h3>
+                : 
+                    <h3>{universityName}</h3>
+                } 
                 <div>
                     <p>
                         {contactName}
