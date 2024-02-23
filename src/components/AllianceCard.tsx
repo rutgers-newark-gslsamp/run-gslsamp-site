825a651d37c018e05c392bdefb1828e5bbba9982
Nicholas Rios <rios.nicholas@protonmail.com>
2024-02-23 14:52:57 -0500

    Move UniversityURL anchor from h3 to Image component

--- src/components/AllianceCard.tsx
+++ src/components/AllianceCard.tsx
@@ -13,25 +13,29 @@ interface AllianceCardProps {
 const AllianceCard: React.FC<AllianceCardProps> = ({ logo, universityName, universityURL, contactName, contactEmail, contactPhone }) => {
     return (
         <div className="bg-white flex flex-col items-center justify-between align-middle p-1 m-1 max-h-56 max-w-56 shadow rounded-lg hover:bg-gray-300 hover:transition duration-500">
-            <Image
-                className="flex justify-center align-middle object-contain p-1 rounded-lg"
-                src={logo}
-                alt={`${universityName} Logo`}
-                width={100}
-                layout="fixed"
-                height={100}
-            />
+            {universityURL ? 
+                <a href={universityURL} target="_blank" className="hover:cursor-pointer hover:underline hover:shadow-gray-2 00 hover:shadow rounded-lg">
+                    <Image
+                        className="flex justify-center align-middle object-contain p-1 rounded-lg"
+                        src={logo}
+                        alt={`${universityName} Logo`}
+                        width={100}
+                        layout="fixed"
+                        height={100}
+                    />
+                </a>
+                :
+                <Image
+                    className="flex justify-center align-middle object-contain p-1 rounded-lg"
+                    src={logo}
+                    alt={`${universityName} Logo`}
+                    width={100}
+                    layout="fixed"
+                    height={100}
+                /> 
+            }   
             <div className="h-1/2 text-sm text-center m-0 p-1 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                {/* if url is null, no cursor click on hover */}
-                {universityURL ? 
-                    <h3>
-                        <a href={universityURL} target="_blank" className="hover:cursor-pointer hover:underline">
-                            {universityName}
-                        </a>
-                    </h3>
-                : 
-                    <h3>{universityName}</h3>
-                } 
+                <h3>{universityName}</h3>
                 <div>
                     <p>
                         {contactName}
