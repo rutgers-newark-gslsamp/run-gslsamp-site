50d858b1be738249af4ede193c385a436b34d8ec
christinolb <49823328+christinolb@users.noreply.github.com>
2024-03-02 11:44:16 -0500

    _blank

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -46,7 +46,7 @@ import EventCard from '../../src/components/EventCard'
                   </div>    
                   <div className="flex justify-evenly items-center">
                       <button className="bg-gray-300 border-4 border-gray-300 rounded text-xs max-w-sm max-h-12 mx-1 shadow-sm shadow-black/40">
-                          <a className="text-base text-black font-semibold hover:underline px-2 py-1" href="https://docs.google.com/forms/d/e/1FAIpQLSdPVNYfpvLnqMP5R4Kl8gzXeqNEDDrcF9bGsIx_HfRMaAFWrA/viewform">Join now!</a>
+                          <a className="text-base text-black font-semibold hover:underline px-2 py-1" href="https://docs.google.com/forms/d/e/1FAIpQLSdPVNYfpvLnqMP5R4Kl8gzXeqNEDDrcF9bGsIx_HfRMaAFWrA/viewform" target='_blank'>Join now!</a>
                       </button>
                       <button className="bg-gray-50 border-4 border-gray-300 rounded text-xs max-w-sm max-h-12 mx-1 shadow-sm shadow-black/40">
                           <a className="text-base text-black font-semibold hover:underline px-2 py-1" href="/events/pastevents">Past Events</a>
