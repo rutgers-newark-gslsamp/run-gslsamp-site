9da427352c5bd55b61bd43fc5f08093924f42c80
Nicholas Rios <rios.nicholas@protonmail.com>
2024-04-19 17:25:27 -0400

    Refactor component to TSX

---
+++ src/components/Title.tsx
@@ -0,0 +1,20 @@
+interface TitleProps {
+    name: string;
+}
+
+const Title: React.FC<TitleProps> = ({ name }) => {
+    return (
+        <>
+            {/* 
+            <h1 className="text-[1.5rem] sm:text-[3.2em] m-[3rem] flex font-bold my-7 drop-shadow-xl">{name}</h1>
+            <hr />
+            */}
+            <div className="section-header">
+                <h1 className="text-4xl uppercase font-extrabold">{name}</h1>
+                <hr />
+            </div>
+        </>
+    );
+};
+
+export default Title;
