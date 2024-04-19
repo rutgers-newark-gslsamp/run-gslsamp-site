a179358969dea8f3ccb8474264ccf8a20d783a68
christinolb <49823328+christinolb@users.noreply.github.com>
2024-04-19 16:06:40 -0400

    faq update

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -28,27 +28,30 @@ function FAQSection() {
   };
   const faqs = [
     {
-      question: "How much does it cost to join?",
-      answer: "RU-N GS-LSAMP is completely free to students and funded by The National Science Foundation."
-    },
-    {
       question: "Who is eligible to participate?",
-      answer: "Minority students at Rutgers Newark in a STEM field."
+      answer: <p>See the participation information <a className='hover:underline' href='/membership' target='_blank'>HERE</a></p>
     },
     {
-      question: "How long is the program, and what is the time commitment?",
-      answer: "Students are required to attend the mandatory meetings which occur a few times throughout the semester." + 
-                "These can range from 30 minutes to 1 hour."
+      question: "At what year can I join GS-LSAMP?",
+      answer: "As early as freshman year. Any student matriculated at Rutgers, Newark, enrolled in or planning to enroll " + 
+                "in a non medical STEM major can apply for the program at any point in their time at Rutgers Newark (some students " +
+                " joined right out of Admitted Student Day as freshman). " +
+                "We encourage you to enter the program as soon as possible, so we can support you as much as possible."
+    },
+    {
+      question: "What is the time commitment for participation?",
+      answer: "To maintain an active status and eligibility for stipends and other opportunities, students must attend several " +
+              " meetings during the semester and meet with the program coordinator twice a semester. Students are encouraged " +
+              " to take advantage of all the offered workshops and opportunities."
     },
     {
       question: "What kind of academic support or tutoring is available through the program?",
-      answer: "Tutoring is always offered in our learning center. In the weekly newsletter you will have access to available" + 
-                " tutors."
+      answer: <p>RUN GSLSAMP offers tutoring based availability and demand, academic bootcamps, professional and career development opportunities,
+         and graduate school application support. Detailed information about resources can be found <a className='hover:underline' href='/membership' target='_blank'>HERE</a></p>
     },
     {
       question: "Are there mentorship or networking opportunities available for participants?",
-      answer: "There are many events hosted every month open to students which give opportunity to network with" + 
-                " other students, staff and guest speakers."
+      answer: <p>There are a plethora of events and opportunities to network, including program meetings, the STEM Conference held every fall, speakers events, and more. To see current upcoming events, visit the <a className='hover:underline' href='/events' target='_blank'>EVENTS PAGE</a></p>
     },
     {
       question: "Does the program provide resources or support for job placement after graduation?",
@@ -57,9 +60,13 @@ function FAQSection() {
     },
     {
       question: "Are there any scholarships or financial aids associated with the program?",
-      answer: "Yes, students can recieve stipends for participating in research and working under a professor."
+      answer: <p>
+                Educational experience stipends are available to active students. Stipends are awarded upon successful completion of research
+                experience with faculty, community outreach and leadership work (as a GS-LSAMP ambassador), Web development through RUN GS-LSAMP, 
+                and several internship opportunities at Rutgers University. Additionally, GS-LSAMP shares many opportunities for financial 
+                resources in the weekly newsletter and other announcements. GS-LSAMP scholars are provided support in the application process.
+              </p>
     },
-    // ... add more FAQ items
   ];
 
   return (
