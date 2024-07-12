c07575a8e0c6023e539a4aedb498fb030bfe5653
gg684 <gg684@scarletmail.rutgers.edu>
2024-07-11 22:13:01 -0400

    Update CarouselVideo.js
    Made the padding "BOX" wider, aligned the arrows and dots. I'll take a look into optimization later.

--- src/components/CarouselVideo.js
+++ src/components/CarouselVideo.js
@@ -18,30 +18,52 @@ const CarouselVideo = ({ videos }) => {
     );
   };
 
+  const goToVideo = (index) => {
+    setCurrentVideoIndex(index);
+  };
+
+
+  {/* Red Padding BOX */}
   return (
-    <div className="flex flex-col items-center bg-gradient-to-t from-red-900 to-red-700 rounded-lg p-4">
-      <div className="z-20 flex flex-col items-center mt-2">
+    <div className="flex flex-col items-center bg-gradient-to-t from-red-900 to-red-700 rounded-lg p-6 relative" style={{ width: '36rem' }}>
+      
+      {/* Indicator dots */}
+      <div className="flex space-x-2 absolute top-5 left-1/2 transform -translate-x-1/2">
+        {videos.map((_, index) => (
+          <span
+            key={index}
+            className={`cursor-pointer ${
+              index === currentVideoIndex ? 'bg-white' : 'bg-gray-400'
+            } w-2 h-2 rounded-full`}
+            onClick={() => goToVideo(index)}
+          ></span>
+        ))}
+      </div>
+
+      <div className="z-20 flex flex-col items-center mt-6"> {/* Increased top margin */}
         <VideoWithLoadingState url={videos[currentVideoIndex]} />
       </div>
 
-      <div className="flex items-center my-4">
+
+      {/* Previous and next buttons */}
+      <div className="absolute top-2 left-0 right-0 flex justify-between items-center px-4">
         <button
           onClick={previousVideo}
-          className="bg-white hover:bg-red-200 text-[#690500] font-bold py-2 px-4 rounded mr-2"
+          className="text-white hover:text-red-200 font-bold text-xl"
         >
-          Previous Video
+          &#9664; {/* LEFT Arrow HTML Symbol */}
         </button>
+
         <button
           onClick={nextVideo}
-          className="bg-white hover:bg-red-200 text-[#690500] font-bold py-2 px-4 rounded"
+          className="text-white hover:text-red-200 font-bold text-xl"
         >
-          Next Video
+          &#9654; {/* RIGHT Arrow HTML Symbol */}
         </button>
       </div>
+
     </div>
   );
 };
 
 export default CarouselVideo;
-
-
