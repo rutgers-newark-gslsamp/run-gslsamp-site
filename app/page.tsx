331b643ebb36c2a308653548c9c717cc64b5fe72
gg684 <gg684@scarletmail.rutgers.edu>
2024-07-24 13:08:18 -0400

    CarouselHero.js and Homepage update!

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,61 @@
-//images
+// Images
 import Image from "next/legacy/image";
 import AllianceGrid from "../src/components/AllianceGrid";
 import LandingAbout from "../src/components/LandingAbout";
 import CtaBlock from "../src/components/CtaBlock";
-import heroPicture from "../public/hero/hero.jpg"
+import heroPicture from "../public/hero/hero.jpg";
+
+import image3 from "../public/club-carousel/image3.jpg"; //Test Image
+
+// Components
+import CarouselHero from "../src/components/CarouselHero";
 
 export default function Home() {
+ 
+  // Array of images for CarouselHero
+  const heroImages = [heroPicture, image3];
+
   return (
     <>
       {/**Title components*/}
-      {/**<Title title={"Index Page"} />*/}
+
+      {/**<Title title={"Index Page"} />*/}  
       <main className="landing-hero-container">
-        {/*Later turn into hero-gallery*/}  
-        <figure className="lg:h-96 xl:h-[32rem] md:h-72 h-44 overflow-hidden ">
-          {/*refactor src */}
-            <Image
-              className="hero-picture object-cover h-full opacity-95"
-              src={heroPicture}
-              alt="Placeholder image"
-            />
+       
+        {/* Hero Gallery */}
+        <figure
+          className="lg:h-96 xl:h-[38rem] md:h-72 h-44 overflow-hidden"
+          style={{
+            marginTop: '-0.99rem', // Horizontal of Hero
+            width: '100%' // Width of Hero
+          }}
+        >
+          <CarouselHero images={heroImages} /> {/* Use CarouselHero */}
         </figure>
-        <CtaBlock />  
+        <CtaBlock />
       </main>
 
+
+      {/* Youtube Video */}
       <section className="flex justify-center my-8 md:my-16 md:mx-8">
-        <iframe className="w-5/6 sm:w-2/3 h-72 md:h-96 lg:h-[28rem]" width="560" height="315" src="https://www.youtube.com/embed/t2HxIdkXPDw?si=_Oo1PBaC7GjsO_95M" title="YouTube video player" /*frameborder="0"*/ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"></iframe>
+        <iframe
+          className="w-5/6 sm:w-2/3 h-72 md:h-96 lg:h-[28rem]"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/t2HxIdkXPDw?si=_Oo1PBaC7GjsO_95M"
+          title="YouTube video player"
+          /* frameborder="0" */
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
+        ></iframe>
       </section>
+
+
+      {/* About */}
       <LandingAbout />
-      <section id='lsamp-alliance-container'>
+      <section id="lsamp-alliance-container">
         <div className="section-header">
-            <h2 className="lsamp-alliance-header">GS-LSAMP Alliance</h2>
-            <hr className="section-horizontal-line"/>
+          <h2 className="lsamp-alliance-header">GS-LSAMP Alliance</h2>
+          <hr className="section-horizontal-line" />
         </div>
         <AllianceGrid />
       </section>
---
+++ src/components/CarouselHero.js
@@ -0,0 +1,43 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+import ImageWithLoadingState from './ImageWithLoadingState';
+
+const CarouselHero = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+
+  const previousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextImage();
+    }, 5000); // Change image every 5 seconds
+
+    return () => {
+      clearInterval(intervalId); // Cleanup the interval on unmount
+    };
+  }, [currentImageIndex]);
+
+  return (
+    <div>
+        <div className="flex justify-center">
+            <ImageWithLoadingState src={images[currentImageIndex]} alt={"Carousel Hero Item"} />
+        </div>
+    </div>
+
+  );
+};
+
+export default CarouselHero;
