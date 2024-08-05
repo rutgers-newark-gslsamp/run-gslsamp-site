a167d175633d47c38e685bafe2e96acc185d77e8
vctrqn19 <a.vic0267@gmail.com>
2024-08-05 15:07:04 -0400

    testing ab us events

--- src/components/EventInfoCard.js
+++ src/components/EventInfoCard.js
@@ -41,7 +41,7 @@ export default function EventInfoCard({isUpcoming}) {
     };
     return (
         <>
-            <div className="flex flex-col sm:flex-row">
+            <div className="grid md:grid-cols-3">
             {loading ? (
                 <p>Loading...</p>
             ) : events === null ? (
