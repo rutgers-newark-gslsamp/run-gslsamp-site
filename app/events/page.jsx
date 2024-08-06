e4e3733663d523a2e046120c2521c1a81f6f61de
vctrqn19 <a.vic0267@gmail.com>
2024-08-06 17:21:04 -0400

    testing dmae

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -39,9 +39,10 @@ import EventCard from '../../src/components/EventCard'
           </div>
 
           {/**NEWSLETTER AND PAST EVENTS*/}
-          <div className='flex items-center'>
-            <section id='newsletter-cta-pc' className='flex justify-center'>
-              <div className="flex flex-col items-center py-3 px-4 ml-10 rounded-3xl shadow-lg max-w-lg shadow-black/20 border">
+          <div className='border-2 flex flex-col items-center'>
+            <section id='newsletter-cta-pc' className='flex flex-col items-center mt-[5%]'>
+              <div className="flex flex-col items-center py-3 px-4 ml-auto sm:ml-10 rounded-3xl shadow-lg max-w-lg shadow-black/20 border">
+                  
                   <div className="flex flex-col items-start text-base">
                       <h2 className="text-4xl font-bold my-1 px-1">Don't Miss An Event!</h2>
                       <p className="p-2 text-base">
@@ -50,6 +51,7 @@ import EventCard from '../../src/components/EventCard'
                         all tailored to fuel your growth. Don't miss out!
                       </p>
                   </div>    
+                  
                   <div className="flex justify-evenly items-center">
                       <button className="bg-gray-300 border-4 border-gray-300 rounded text-xs max-w-sm max-h-12 mx-1 shadow-sm shadow-black/40">
                           <a className="text-base text-black font-semibold hover:underline px-2 py-1" href="/membership" target='_blank'>Join now!</a>
@@ -58,6 +60,7 @@ import EventCard from '../../src/components/EventCard'
                           <a className="text-base text-black font-semibold hover:underline px-2 py-1" href="/events/pastevents" target='_blank'>Past Events</a>
                       </button>
                   </div>
+
               </div>
             </section>
           </div>
