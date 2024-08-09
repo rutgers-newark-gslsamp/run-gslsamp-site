ee6e7cb83a3f6f93d45a25b3c0c28b337c112a30
gg684 <gg684@scarletmail.rutgers.edu>
2024-08-09 18:34:55 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ import CarouselHero from "../src/components/CarouselHero";
 export default function Home() {
  
   // Array of images for CarouselHero
-  const heroImages = [image11, image22, image33, image44]; //image1 vs. image11 with different dimensions
+  const heroImages = [image22, image11, image33, image44]; //image1 vs. image11 with different dimensions
 
   return (
     <>
